19043378c8e1cafc662730cc16e002f2ec56de6b
Gianmaria <scogiam95@gmail.com>
2021-01-21 00:31:03 +0100

    added gearman-worker

---
+++ gearman-worker/worker.js
@@ -0,0 +1,29 @@
+const gearman = require('gearman')
+
+let worker = gearman()
+
+// handle jobs assigned by the server
+worker.on('JOB_ASSIGN', function(job) {
+    console.log(job.func_name + ' job assigned to this worker')
+    // create a buffer
+    const buff = Buffer.from(job.payload.toString(), 'utf-8');
+    // decode buffer as Base64
+    const result = buff.toString('base64');
+    // notify the server the job is done
+    worker.sendWorkComplete(job.handle, result)
+
+    // go back to sleep, telling the server we're ready for more work
+    worker.preSleep()
+});
+
+// grab a job when the server signals one is available
+worker.on('NOOP', function() {  worker.grabJob() })
+
+// connect to the gearman server
+worker.connect(function(){
+    // register the functions this worker is capable of
+    worker.addFunction('encode')
+
+    // tell the server the worker is going to sleep, waiting for work
+    worker.preSleep()
+});
